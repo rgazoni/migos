09dbda8c4f9e8cf7badd0bc0939c5face39ce3f7
EduardoFujiki <eduardofmaeda@hotmail.com>
2023-08-30 20:24:47 -0300

    signup express validator

--- src/Auth/routes/signin.ts
+++ src/Auth/routes/signin.ts
@@ -1,9 +1,22 @@
 import express, { Response, Request } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
+
 
 const router = express.Router()
 
 router.post(
     '/api/users/signin',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must supply a password')
+    ],
+    validateRequest,
     async (req: Request, res: Response) => {
         const { email, password } = req.body;
 
--- src/Auth/routes/signup.ts
+++ src/Auth/routes/signup.ts
@@ -1,7 +1,6 @@
 import express, { Response, Request } from 'express';
-import { body } from 'express-validator'
+import { body } from 'express-validator';
 import { validateRequest } from '../middlewares/validate-request';
-
 
 const router = express.Router();
 
@@ -13,19 +12,27 @@ router.post(
             .withMessage('Email must be valid'),
         body('password')
             .trim()
-            .isStrongPassword({minLength:8,minUppercase:1,minNumbers:1,minSymbols:1})
-            
-            .withMessage('Password must be between 4 and 20 characters')
+            .isStrongPassword({ minLength: 8, minUppercase: 1, minNumbers: 1, minSymbols: 1 })
+            .withMessage('Password must have at least eight characters being at least one uppercase, one number and one special character'),
+        body('birthDate')
+            .isISO8601()
+            .notEmpty()
+            .withMessage('You must supply a correct birth date'),
+        body('firstName')
+            .matches("^[A-Za-zãáâéêíôõóú\s]+$")
+            .notEmpty()
+            .withMessage('You must supply a correct name'),
+        body('lastName')
+            .matches("^[A-Za-zãáâéêíôõóú\s]+$")
+            .notEmpty()
+            .withMessage('You must supply a correct last name'),
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-        const { email, password } = req.body;
+        const { email, password, birthDate, firstName, lastName } = req.body;
 
-        
-        res.status(200).send({ email, password });
+        res.status(200).send({ email, password, birthDate, firstName, lastName });
     }
-    
 );
 
-
-export { router as signupRouter }
+export { router as signupRouter };
