db6ce8c6c846bc0c806b16b5198f1d62bef19409
Victor Fukumoto <takeshi.fukumoto2002@gmail.com>
2023-09-12 19:15:02 -0300

    userAuth interaction model

--- src/Auth/routes/signup.ts
+++ src/Auth/routes/signup.ts
@@ -1,4 +1,5 @@
 import express, { Response, Request } from 'express';
+import { NewUser } from '../models/NewUser';
 import { body } from 'express-validator';
 import { validateRequest } from '../middlewares/validate-request';
 
@@ -13,7 +14,6 @@ router.post(
         body('password')
             .trim()
             .isStrongPassword({ minLength: 8, minUppercase: 1, minNumbers: 1, minSymbols: 1 })
-   signin-expressvalidator
             .withMessage('Password must have at least eight characters being at least one uppercase, one number and one special character'),
         body('birthDate')
             .isISO8601()
@@ -31,6 +31,21 @@ router.post(
     validateRequest,
     async (req: Request, res: Response) => {
         const { email, password, birthDate, firstName, lastName } = req.body;
+        let user = new NewUser();
+        let status;
+        
+        await user.initialize();
+
+        if(await user.exists(email)){
+            //retornar errode usuario ja existente
+            console.log("ja existe ;-;");
+            res.status(404).send("ja existe ;-;");
+            return;
+        }
+
+        await user.create(email, password, birthDate, firstName, lastName);
+        user.close();
+
 
         res.status(200).send({ email, password, birthDate, firstName, lastName });
     }
